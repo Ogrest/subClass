4837e29c37315871b20c0b8e6c779d7f93074c2b
Andrew <dondoex@gmail.com>
2022-06-24 18:15:08 +0400

    Lj,fdktybt

---
+++ src/src/BaseFigura.java
@@ -0,0 +1,14 @@
+package src;
+
+public class BaseFigura {
+    int x;
+    int y;
+
+    public BaseFigura(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public static void area(int x, int y){
+        System.out.println("Площадь фигуры: " + x * y);
+    }
+}
